695a8123945b9c4f82c16991bc039fa1424ef2da
Chance Vodnoy <esgameco@gmail.com>
2021-03-12 19:51:32 -0800

    Changed setup to new version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
    name='youtube-upload-monetization',
-   version='1.2',
+   version='1.4',
    description='Uploads videos to youtube through the use of selenium',
    author='Chance Vodnoy',
    author_email='chancevodnoy129@gmail.com',
